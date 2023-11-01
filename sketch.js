532ec86a222bf1f9bb8e2460fc6cb0335bb1b100
Pdro1208 <148362194+Pdro1208@users.noreply.github.com>
2023-11-01 11:44:32 -0300

    adicionando arquivos html js e css

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let cor;
+let circleX;
+let circleY;
+
+function setup() {
+  createCanvas(400, 400);
+background(100, 0, 0);
+cor=color(random (255), random(255),random(255))
+circuloX= [0, 0];
+circuloY= [random(height),random (height)];
+}
+
+function draw() {
+  fill(cor);
+ for(let contador in circuloX) {   
+  circle(circuloX[0], circuloY[0], 50);
+  circuloX[contador] += random(0, 3) 
+  circuloY[contador] += random(-3, 3)
+   if(circuloX[contador] >= width) {
+     circuloX[contador] = 0;
+     circuloY[contador] = random (height);
+   }
+   
+   
+   if (mouseIsPressed) {
+    cor =color ( random (0,255), random(0, 255), random (0,255), random (0,100));
+  }
+}
+}
